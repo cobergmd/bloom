7ec39d1db260332d8e46ac91283d72530cc1b0d7
Craig Oberg <coberg@ssicorp.socialsolutions.com>
2016-10-20 11:25:51 -0400

    first red tdd test for adding words

--- bloom/bloom.test.js
+++ bloom/bloom.test.js
@@ -1,5 +1,7 @@
 var test = require('tape');
+var bloom = require('bloom');
 
 test('add a word to the dictionary', function (t) {
-
+    bloom.addWord('apple');
+    t.equal(bloom.wordCount() === 1);
 });
