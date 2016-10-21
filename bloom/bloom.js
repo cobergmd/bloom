d0849c303ade0a05416793b929946679062bdece
Craig Oberg <coberg@ssicorp.socialsolutions.com>
2016-10-21 08:36:02 -0400

    added a hash function

--- bloom/bloom.js
+++ bloom/bloom.js
@@ -12,4 +12,20 @@ Bloom.prototype.exists = function (word) {
     return (this.words.includes(word) > 0);
 }
 
+// Naive implementation of djb2 
+// http://www.cse.yorku.ca/~oz/hash.html
+// Using 32 bit numbers where as above link is using 64 bits (unsigned long)
+Bloom.prototype.hash = function (word) {
+    var hash = 5381;
+    var wlen = word.length;
+
+    for (var i = 0; i < wlen; i++) {
+        var c = word.charCodeAt(i);
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash & hash;  // bitwise ops return 32 bit numbers
+}
+
+
+
 module.exports = Bloom;
--- bloom/bloom.test.js
+++ bloom/bloom.test.js
@@ -3,7 +3,7 @@ var bloom = require('./bloom');
 
 let dict = new bloom();
 
-test('add a word to the dictionary', function (t) {
+test('add a word to the dictionary', function(t) {
     dict.add('apple');
     t.equal(dict.exists('apple'), true);
     t.end();
@@ -13,4 +13,14 @@ test('check that a word does not exist in the dictionary', function(t) {
     t.equal(dict.exists('xykadjsoinv'), false);
     t.end();
 });
+
+test('hash a string value', function(t) {
+    var hash1 = dict.hash('asdfqwerzxcv');
+    var hash2 = dict.hash('Supercalifragilisticexpialidocious');
+    var hash3 = dict.hash('TtUuOoMmDd');
+    t.equal(dict.hash('asdfqwerzxcv'), hash1);
+    t.equal(dict.hash('Supercalifragilisticexpialidocious'), hash2);
+    t.equal(dict.hash('TtUuOoMmDd'), hash3);
+    t.end();
+})
 
