52a4d0db2075106e28189f7f742e07ba9803aac5
Craig Oberg <coberg@ssicorp.socialsolutions.com>
2016-10-21 15:43:51 -0400

    changed hashing scheme altered hash function to return two 32 bit hashes for input to double hash function. added double hash function.

--- bloom/bloom.js
+++ bloom/bloom.js
@@ -12,20 +12,28 @@ Bloom.prototype.exists = function (word) {
     return (this.words.includes(word) > 0);
 }
 
-// Naive implementation of djb2 
+// Naive 32 bit implementation of djb2 
 // http://www.cse.yorku.ca/~oz/hash.html
-// Using 32 bit numbers where as above link is using 64 bits (unsigned long)
+// Returns two hashes seeded with different 32 bit primes
 Bloom.prototype.hash = function (word) {
-    var hash = 5381;
+    var hashes = [];
+    var hash1 = 5381;
+    var hash2 = 31907;
     var wlen = word.length;
 
     for (var i = 0; i < wlen; i++) {
         var c = word.charCodeAt(i);
-        hash = ((hash << 5) + hash) + c;
+        hash1 = ((hash1 << 5) + hash1) + c;
+        hash2 = ((hash2 << 5) + hash2) + c;
     }
-    return hash & hash;  // bitwise ops return 32 bit numbers
+    hashes.push(hash1 & hash1);  // bitwise ops return 32 bit numbers
+    hashes.push(hash2 & hash2);  
+    return hashes;
 }
 
-
+// https://en.wikipedia.org/wiki/Double_hashing
+Bloom.prototype.doubleHash = function (count, hash1, hash2, size) {
+    return (hash1 + count * hash2) % size;
+}
 
 module.exports = Bloom;
--- bloom/bloom.test.js
+++ bloom/bloom.test.js
@@ -18,9 +18,19 @@ test('hash a string value', function(t) {
     var hash1 = dict.hash('asdfqwerzxcv');
     var hash2 = dict.hash('Supercalifragilisticexpialidocious');
     var hash3 = dict.hash('TtUuOoMmDd');
-    t.equal(dict.hash('asdfqwerzxcv'), hash1);
-    t.equal(dict.hash('Supercalifragilisticexpialidocious'), hash2);
-    t.equal(dict.hash('TtUuOoMmDd'), hash3);
+    t.equal(dict.hash('asdfqwerzxcv')[0], hash1[0]);
+    t.equal(dict.hash('asdfqwerzxcv')[1], hash1[1]);
+    t.equal(dict.hash('Supercalifragilisticexpialidocious')[0], hash2[0]);
+    t.equal(dict.hash('Supercalifragilisticexpialidocious')[1], hash2[1]);
+    t.equal(dict.hash('TtUuOoMmDd')[0], hash3[0]);
+    t.equal(dict.hash('TtUuOoMmDd')[1], hash3[1]);
     t.end();
-})
+});
+
+test('generate additional hashes', function(t) {
+    var hash = dict.hash('Supercalifragilisticexpialidocious');
+    var bloom = dict.doubleHash(1, hash[0], hash[1], 128);
+    t.equal(bloom, dict.doubleHash(1, hash[0], hash[1], 128));
+    t.end();
+});
 
