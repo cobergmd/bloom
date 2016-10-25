f9a426acafab60f819e7e3d555a5a3b5c3657fcf
Craig Oberg <coberg@ssicorp.socialsolutions.com>
2016-10-25 10:54:59 -0400

    added more tests new tests to check for too many false positives and ensure there are no false negatives.

--- bloom/bloom.test.js
+++ bloom/bloom.test.js
@@ -1,6 +1,14 @@
 var test = require('tape');
 var bloom = require('./bloom');
 
+let commonWords = ['time','person','year','way','day','thing','man','world','life',
+    'hand','part','child','eye','woman','place','work','week','case','point',
+    'government','company','number','group','problem','fact'] 
+let moreWords = ['airplane','driver','throw','trail','understanding','plenty',
+    'newspaper','chapter','Atlantic','Pacific','chapter','Chinese','Russian',
+    'German','Dutch','grain','James','zoo','wealth','promised','damage','cookies',
+    'brick','acres','arrangement'] 
+
 let dict = new bloom();
 
 test('add a word to the dictionary', function(t) {
@@ -40,5 +48,43 @@ test('get filter slot after double hashing values', function(t) {
     t.notEqual(dblHash1, dblHash2);
     t.end();
 });
+
+test('simple check for false negatives with 128/5 filter', function(t) {
+    var dict = new bloom();
+    for (var i = 0; i < commonWords.length; i++) {
+        dict.add(commonWords[i]);
+    }
+    for (var j = 0; j < commonWords.length; j++) {
+        t.notEqual(dict.exists(commonWords[j]), false, commonWords[j]);
+    }
+    t.end();
+})
+
+test('simple check for false negatives with 50/3 filter', function(t) {
+    var dict = new bloom(50,3);
+    for (var i = 0; i < commonWords.length; i++) {
+        dict.add(commonWords[i]);
+    }
+    for (var j = 0; j < commonWords.length; j++) {
+        t.notEqual(dict.exists(commonWords[j]), false, commonWords[j]);
+    }
+    t.end();
+})
+
+test('test for an execessively high false positive count', function(t) {
+    var dict = new bloom(512,3);  // a 512/3 filter was required to pass this test
+    var hits = 0;
+    for (var i = 0; i < commonWords.length; i++) {
+        dict.add(commonWords[i]);
+    }
+    for (var j = 0; j < moreWords.length; j++) {
+        if (dict.exists(moreWords[j]) === true) {
+            hits++;
+        }
+    }
+    var hitRate = hits / moreWords.length;
+    t.ok(hitRate < .1, 'words:' + moreWords.length + ' hits:' + hits + ' = ' + hitRate);
+    t.end();
+})
 
 
