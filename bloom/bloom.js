7970d7c9df5818dce8658e8886368afd976f54f3
Craig Oberg <coberg@ssicorp.socialsolutions.com>
2016-10-25 13:16:15 -0400

    add wordlist module wordlist will load a large word file and use a bloom filter to allow for checking if a word exists.

--- bloom/bloom.js
+++ bloom/bloom.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// This node module is a simple bloom filter that can be used to maintain a 
+// bitmap for determining if an entry already exists or not in a word list.
+// This implementation is using a boolean array to respresent the bitmap.
+// Parameters:
+// size - The length of the filter, defaults to 128 bytes.
+// count - The number of times the value will be hashed.
 function Bloom(size, count) {
     this.filterSize = size || 128;
     this.hashCount = count || 5;
@@ -9,6 +15,7 @@ function Bloom(size, count) {
     }
 }
 
+// Add a string value to the dictionary
 Bloom.prototype.add = function (word) {
     var hash = this.hash(word);
     for (var i = 0; i < this.hashCount; i++) {
@@ -16,6 +23,8 @@ Bloom.prototype.add = function (word) {
     }
 }
 
+// Determine if a string value has already been added.  
+// False positives are possible but false negatives should not occur.
 Bloom.prototype.exists = function (word) {
     var hash = this.hash(word);
     for (var i = 0; i < this.hashCount; i++) {
@@ -26,6 +35,8 @@ Bloom.prototype.exists = function (word) {
     return false;
 }
 
+// Creates a string representation of the bloom filter bitmap.  
+// Each false value is a dash and each true value is an x.
 Bloom.prototype.toString = function () {
     var result = [];
     for (var i = 0; i < this.filterSize; i++) {
@@ -41,6 +52,7 @@ Bloom.prototype.toString = function () {
 // Naive 32 bit implementation of djb2 
 // http://www.cse.yorku.ca/~oz/hash.html
 // Returns two hashes seeded with different 32 bit primes
+// to mimic a 64 bit response.
 Bloom.prototype.hash = function (word) {
     var hashes = [];
     var hash1 = 5381;
@@ -59,6 +71,7 @@ Bloom.prototype.hash = function (word) {
 
 // Return the corresponding bloom filter slot for the hashed value 
 Bloom.prototype.getSlot = function (count, hash) {
+    // Math for generating additional hashes from two original hashes
     // https://en.wikipedia.org/wiki/Double_hashing
     return (hash[0] + count * hash[1]) % this.filterSize;
 }
---
+++ bloom/wordlist.test.js
@@ -0,0 +1,8 @@
+var test = require('tape');
+var WordList = require('wordList');
+
+test('load dictionary from a word list', function(t) {
+    let list = new WordList();
+    t.ok(list.contains("apple"));
+    t.end();
+});
